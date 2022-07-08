2694fe393893d2274b432b9f30c3d2ee7c37ecbd
Prajwal Iyer <prajwaliyer@tamu.edu>
2022-07-09 02:23:20 +0300

    Weather POST request error handling

--- src/components/Views/Trips/AddTrips.js
+++ src/components/Views/Trips/AddTrips.js
@@ -28,7 +28,25 @@ const AddTrips = ({ onAdd }) => {
   const onSubmit = (e) => {
     e.preventDefault();
     let item = { name, country, temp, humidity, main, icon };
-    API.post("weather/", item).then(() => refreshCities());
+    API.post("weather/", item).then(() => refreshCities())
+    .catch(function (error) {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log('Error', error.message);
+      }
+      console.log(error.config);
+    });
     console.log("Custom: POST request sent");
   };
 
