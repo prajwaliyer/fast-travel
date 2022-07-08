f8435270d20d5b8313b30b3b9fd8b694f191f60e
Prajwal Iyer <prajwaliyer@tamu.edu>
2022-07-09 01:53:17 +0300

    Update API baseURL

--- src/API.js
+++ src/API.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: "http://127.0.0.1:8000/api/",
+    baseURL: "https://fast-travel-staging.herokuapp.com/api/",
     headers: {
         'Accept':'application/json',
         'Content-Type':'application/json',
