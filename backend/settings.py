69a803577eb6f7b20d7ce8ce49a6f76e06d75ccb
Prajwal Iyer <prajwaliyer@tamu.edu>
2022-07-08 21:20:03 +0300

    Deployment files 5

--- backend/settings.py
+++ backend/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['fast-travel.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 CORS_ORIGIN_ALLOW_ALL = True
 
 
