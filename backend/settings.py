ef0cbe22d9d20b9b8230c3b52c140e2dd4ce4f38
akash-gajendra <akash.gajendra@tamu.edu>
2022-07-11 22:07:27 +0400

    Migration to PostgreSQL  - Finalized Production
    Removed data.json, sqlite files and restored postgresql connection in settings

--- backend/settings.py
+++ backend/settings.py
@@ -31,7 +31,7 @@ if os.path.isfile(dotenv_file):
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
@@ -108,21 +108,14 @@ POSTGRESQL_PASSWORD = os.environ['POSTGRESQL_PASSWORD']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db525h8177pluq',
+        'USER': 'lqbefkkhhxqeoq',
+        'PASSWORD': str(POSTGRESQL_PASSWORD),
+        'HOST': 'ec2-54-159-22-90.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'db525h8177pluq',
-#         'USER': 'lqbefkkhhxqeoq',
-#         'PASSWORD': str(POSTGRESQL_PASSWORD),
-#         'HOST': 'ec2-54-159-22-90.compute-1.amazonaws.com',
-#         'PORT': '5432',
-#     }
-# }
 
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
